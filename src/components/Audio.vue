999a61bd6ad73c3bd45c8e434341f2f4ddd9021b
Pankaj shah <shahpankaj981@gmail.com>
2019-09-19 17:28:01 +0545

    changed proxy path

--- src/components/Audio.vue
+++ src/components/Audio.vue
@@ -201,7 +201,7 @@ export default {
       this.$http.get(this.baseurl + '/audio/' + this.savedAudioFile)
         .then(response => {
           console.log('audio url', response)
-          self.audioFullPath = 'http://localhost:8080' + response.data.audio_url
+          self.audioFullPath = 'http://3.16.193.35:5001' + response.data.audio_url
         })
         .catch(error => {
           return ''
